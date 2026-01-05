2383d89bf360cdf96cb1f5ebe1bd7f11a4d8e4b5
Evan Burkeen <evanburkeen@Evans-MacBook-Air-2773.local>
2026-01-05 17:13:25 -0500

    Remove basePath for subdomain

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: '/flights',
   env: {
     SERP_API_KEY: process.env.SERP_API_KEY,
     ANTHROPIC_API_KEY: process.env.ANTHROPIC_API_KEY,
